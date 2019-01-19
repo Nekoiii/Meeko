017fb7498372ceacd1ca116021f751c8271b6f69
Nekoi <10nekosa@gmail.com>
2019-01-20 00:30:58 +0900

    Meeko

--- Meeko/Meeko/people.js
+++ Meeko/Meeko/people.js
@@ -8,12 +8,12 @@ var canvas_y = 1000;
 function fromAngleMakeRad(angle_min, angle_max) {
     let ran_min = angleToRadian(angle_min);
     let ran_max = angleToRadian(angle_max);
-    return (Math.random() * (ran_max - ran_min) + ran_min);
+    return (Math.random() * (ran_max * 10 - ran_min * 10) + ran_min * 10) / 10;
 }
 
 //random
 function makeRandom(min, max) {
-    return (Math.random() * (max - min) + min);
+    return (Math.random() * (max - min + 1) + min);
 }
 
 //角度转换弧度
@@ -56,10 +56,9 @@ function Joint(preJoint, len, r, rad_xy, color, ratio) {
     this.color = color;
     this.rad_xy = rad_xy;//自定义角度规则:角度从x轴开始顺时针转
     this.len = len;
-    this.r = (this.ratio + 0.5) * r;//注:cos()、sin()用的是弧度不是角度
-    this.x = preJoint.x + (this.ratio *0.5* len+0.5*len) * Math.cos(rad_xy);
-    this.y = preJoint.y + (this.ratio * 0.5 * len + 0.5 * len) * Math.sin(rad_xy);
-    console.log(this.name,"Coordinate: ", this.x, this.y, this.ratio);
+    this.r = (this.ratio + 0.5) * r;  //r的范围: [ratio , ratio + 0.5]
+    this.x = preJoint.x + (this.ratio * 0.7 * len + 0.3 * len) * Math.cos(rad_xy);//注:cos()、sin()用的是弧度不是角度
+    this.y = preJoint.y + (this.ratio * 0.7 * len + 0.3 * len) * Math.sin(rad_xy);
     this.drawJoint = function () {
         var canvas = document.getElementById("canvas");//画关节
         var pen = canvas.getContext("2d");
@@ -69,6 +68,7 @@ function Joint(preJoint, len, r, rad_xy, color, ratio) {
         pen.moveTo(preJoint.x, preJoint.y);//连线
         pen.lineTo(this.x, this.y);
         pen.stroke();
+        console.log(this.name, "Coordinate: ", this.x, this.y, this.ratio, this.rad_xy);
     }
 }
 
@@ -78,7 +78,7 @@ function Head(x, y) {
     this.y = y;
     this.r = 40;
     //this.ratio = makeRatio(0);
-    this.color = 'green';
+    this.color = '#DD6666';
     this.drawJoint = function () {//画关节
         var canvas = document.getElementById("canvas");
         var pen = canvas.getContext("2d");
@@ -96,8 +96,8 @@ function Neck(preJoint) {
     this.len = 60;
     this.r = 10;
     this.rad_xy = fromAngleMakeRad(30, 150);
-    this.ratio = makeRatio(0);
-    this.color = 'red';
+    this.ratio = makeRatio(1);
+    this.color = '#A07050';
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
 }
 
@@ -107,14 +107,9 @@ function Shoulder(preJoint, ifLeft) {
     this.preJoint = preJoint;
     this.len = 60;
     this.r = 10;
-    if (ifLeft) {
-        this.rad_xy = fromAngleMakeRad(-60, 30);
-    }
-    else {
-        this.rad_xy = fromAngleMakeRad(150, 240);
-    }
-    this.ratio = makeRatio(0);
-    this.color = 'blue';
+    this.rad_xy = ifLeft ? fromAngleMakeRad(-30, 30) : fromAngleMakeRad(150, 210);
+    this.ratio = makeRatio(1);
+    this.color = '#905070';
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
 }
 
@@ -126,7 +121,7 @@ function Elbow(preJoint) {
     this.r = 10;
     this.rad_xy = fromAngleMakeRad(0, 360);
     this.ratio = makeRatio(0);
-    this.color = 'orange';
+    this.color = '#9055A0';
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
 }
 
@@ -138,7 +133,7 @@ function Wrist(preJoint) {
     this.r = 10;
     this.rad_xy = fromAngleMakeRad(0, 360);
     this.ratio = makeRatio(0);
-    this.color = '#990099';
+    this.color = '#C090DD';
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
 }
 
@@ -148,7 +143,7 @@ function Waist(preJoint) {
     this.preJoint = preJoint;
     this.len = 150;
     this.r = 8;
-    this.color = '#22002C';
+    this.color = '#60AA50';
     this.rad_xy = fromAngleMakeRad(30, 150);
     this.ratio = makeRatio(0);
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
@@ -160,9 +155,9 @@ function Hip_center(preJoint) {
     this.preJoint = preJoint;
     this.len = 30;
     this.r = 8;
-    this.color = '#00CC10';
+    this.color = '#209020';
     this.rad_xy = fromAngleMakeRad(30, 150);
-    this.ratio = makeRatio(0);
+    this.ratio = makeRatio(1);
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
 }
 //髋部类
@@ -171,37 +166,73 @@ function Hip(preJoint, rad_xy) {
     this.preJoint = preJoint;
     this.len = 50;
     this.r = 12;
-    this.color = '#ACCF30';
+    this.color = '#806020';
     this.rad_xy = rad_xy;
-    this.ratio = makeRatio(0);
+    this.ratio = makeRatio(1);
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
 }
 
 //膝盖类
-function Knee(preJoint, ifLeft, ratio) {
+function Knee(preJoint, ifLeft, ratio, flag, flag_2) {
     this.name = 'Knee';
     this.preJoint = preJoint;
     this.len = 200;
     this.r = 10;
-    this.color = '#CC5010';
-    if (ifLeft) {
-        this.rad_xy = fromAngleMakeRad(-60, 210);
-    }
-    else {
-        this.rad_xy = fromAngleMakeRad(-30, 240);
+    this.color = '#AA8060';
+    let x = makeRandom(1, 10);
+    switch (flag) {
+        case 0: {  //flag=0:站立
+            if (x > 3) {
+                this.rad_xy = ifLeft ? fromAngleMakeRad(30, 120) : fromAngleMakeRad(60, 150);
+            }
+            else {
+                this.rad_xy = ifLeft ? fromAngleMakeRad(-60, 210) : fromAngleMakeRad(-30, 240);
+            }
+            break;
+        }
+        case 1: {//坐下/跪着or盘腿
+            this.rad_xy = flag_2 ? fromAngleMakeRad(0, 180) : fromAngleMakeRad(180, 360);
+            break;
+        }
+        default:
+            break;
     }
     this.ratio = ratio;
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
 }
 
 //脚踝类
-function Ankle(preJoint) {
+function Ankle(preJoint, ifLeft, flag, flag_2) {
     this.name = 'Ankle';
     this.preJoint = preJoint;
     this.len = 195;
-    this.r = 8;
-    this.color = '#33EEAA';
-    this.rad_xy = fromAngleMakeRad(0, 360);
+    this.r = 10;
+    this.color = '#DDA090';
+    //this.rad_xy = fromAngleMakeRad(0, 360);
+    let knee_rad_xy = preJoint.rad_xy;
+    switch (flag) {
+        case 0: {  //flag=0:站立
+            this.rad_xy = knee_rad_xy + fromAngleMakeRad(-45, 45);
+            break;
+        }
+        case 1: {//坐下
+            if (flag_2) {
+                this.rad_xy = knee_rad_xy + ((knee_rad_xy > angleToRadian(90) && knee_rad_xy < angleToRadian(270)) ?
+                    fromAngleMakeRad(-150, -45) : fromAngleMakeRad(45, 150));
+            }
+            else {
+                this.rad_xy = knee_rad_xy + ((knee_rad_xy > angleToRadian(90) && knee_rad_xy < angleToRadian(270)) ?
+        fromAngleMakeRad(-45, 0) : fromAngleMakeRad(0, 45));
+            }
+            break;
+        }
+        case 2: {//跪着or盘腿
+            this.rad_xy = knee_rad_xy + (ifLeft ? fromAngleMakeRad(120, 180) : fromAngleMakeRad(-120, -180));
+            break;
+        }
+        default:
+            break;
+    }
     this.ratio = makeRatio(0);
     Joint.call(this, preJoint, this.len, this.r, this.rad_xy, this.color, this.ratio);
 }
@@ -210,7 +241,7 @@ function Ankle(preJoint) {
 //People类
 function People(flag) {
     let r_1 = 40, r_2 = 10;
-    this.head = new Head(makeRandom(200, 800), makeRandom(100, 400));
+    this.head = new Head(makeRandom(200, 600), makeRandom(100, 500));
     this.neck = new Neck(this.head);
     this.shoulder_R = new Shoulder(this.neck, 0);
     this.shoulder_L = new Shoulder(this.neck, 1);
@@ -227,15 +258,45 @@ function People(flag) {
         case 0: {  //flag=0:站立
             let ratio_Knee_R = makeRatio(1);
             let ratio_Knee_L = makeRatio(1);
-            this.Knee_R = new Knee(this.Hip_R, 0, ratio_Knee_R);
-            this.Knee_L = new Knee(this.Hip_L, 1, ratio_Knee_L);
-            this.Ankle_R = new Ankle(this.Knee_R);
-            this.Ankle_L = new Ankle(this.Knee_L);
+            this.Knee_R = new Knee(this.Hip_R, 0, ratio_Knee_R, flag, 0);
+            this.Knee_L = new Knee(this.Hip_L, 1, ratio_Knee_L, flag, 0);
+            this.Ankle_R = new Ankle(this.Knee_R, 0, flag, 0);
+            this.Ankle_L = new Ankle(this.Knee_L, 1, flag, 0);
             break;
         }
         case 1: {//坐下
-
+            let x = makeRandom(1, 10);
+            let y = makeRandom(1, 10);
+            let z = makeRandom(1, 10);
+            let ratio_Knee_R;
+            let ratio_Knee_L;
+            if (x > 6) {
+                ratio_Knee_R = makeRatio(2);
+                ratio_Knee_L = makeRatio(2);
+            }
+            else {
+                ratio_Knee_R = makeRatio(3);
+                ratio_Knee_L = makeRatio(3);
+            }
+            let ankle_flag_2 = (y > 2) ? 1 : 0;
+            let knee_flag_2 = (z > 6) ? 1 : 0;
+            this.Knee_R = new Knee(this.Hip_R, 0, ratio_Knee_R, flag, knee_flag_2);
+            this.Knee_L = new Knee(this.Hip_L, 1, ratio_Knee_L, flag, knee_flag_2);
+            this.Ankle_R = new Ankle(this.Knee_R, 0, flag, ankle_flag_2);
+            this.Ankle_L = new Ankle(this.Knee_L, 1, flag, ankle_flag_2);
+            break;
         }
+        case 2: {//跪着or盘腿
+            let ratio_Knee_R = makeRatio(0);
+            let ratio_Knee_L = makeRatio(0);
+            this.Knee_R = new Knee(this.Hip_R, 0, ratio_Knee_R, 1, 0);
+            this.Knee_L = new Knee(this.Hip_L, 1, ratio_Knee_L, 1, 0);
+            this.Ankle_R = new Ankle(this.Knee_R, 0, flag, 0);
+            this.Ankle_L = new Ankle(this.Knee_L, 1, flag, 0);
+            break;
+        }
+        default:
+            break;
     }
     this.drawPoint = function () {//画关节
         this.head.drawJoint();
